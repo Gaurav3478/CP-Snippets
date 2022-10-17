6cff0178f463756efbbd87aab5168574882676e7
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-10-17 17:35:51 +0530

    Update modpow.cpp

--- modpow.cpp
+++ modpow.cpp
@@ -1,15 +1,15 @@
-ll modpow(ll b, n, m) {
-	if(n == 0) {
-		return 1;
-	}
-	if(n == 1) {
-		return b;
-	}
-	ll x = modpow(b, n/2, m);
-	if(n%2 == 0) {
-		return (x*x)%m;
-	}
-	else {
-		return (b*x*x)%m;
-	}
+ll modpow(ll b, ll n, ll m) {
+    if(n == 0) {
+        return 1;
+    }
+    if(n == 1) {
+        return b;
+    }
+    ll x = modpow(b, n/2, m);
+    if(n%2 == 0) {
+        return (x*x)%m;
+    }
+    else {
+        return (b*x*x)%m;
+    }
 }
