f307d746658bc567b1600b5e4d1828ed6d9c0b26
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2022-12-13 15:11:19 +0530

    Update primeFactorization.cpp

--- primeFactorization.cpp
+++ primeFactorization.cpp
@@ -1,26 +1,30 @@
-ll MAXN = 1e7 + 5;
-vector<ll> spf(MAXN + 1, 1);
+int MAXN = 1e7 + 5;
+vector<int> spf(MAXN + 1, 1);
 
 void Sieve() {
-    for(ll i = 2, i <= MAXN, i++) {
-        if (spf[i] == 1) {
-            spf[i] = i;
-            for (ll j = i * i; j <= N; j += i) {
-                if (spf[j] == 1) {
+    for(int i = 2; i <= MAXN; i+=2) {
+        spf[i]=2;
+    }
+
+    for(int i = 3; i <= MAXN; i+=2) {
+        if(spf[i] == 1) {
+            for(int j = i; j <= MAXN; j += i) {
+                if(spf[j] == 1) {
                     spf[j] = i;
                 }
+            }
         }
-    }
+   }
 }
 
-vector<ll> PrimeFactorization(ll n) {
-    vector<ll> primes;
+vector<int> primeFactorization(int n) {
+    vector<int> primes;
     while (n > 1) {
-        ll fact = spf[n];
+        int fact = spf[n];
         while (n % fact == 0) {
             n /= fact;
         }
         primes.push_back(fact);
     }
-    return ans;
+    return primes;
 }
