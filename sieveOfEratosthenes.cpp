63fa39c5b9ab67199f7a5fad590b7c578d34bf24
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2023-07-02 10:26:53 +0530

    Update sieveOfEratosthenes.cpp

--- sieveOfEratosthenes.cpp
+++ sieveOfEratosthenes.cpp
@@ -1,17 +1,17 @@
 vector<int> primes;
 vector<bool> is_prime;
 
-void Sieve() {
-    is_prime.resize(N + 1, true);
+void Sieve(int n) {
+    is_prime.resize(n + 1, true);
     is_prime[0] = is_prime[1] = false;
-    for (int i = 2; i <= N; i++) {
-        if (is_prime[i] && (ll)i * i <= N) {
-            for (int j = i * i; j <= N; j += i)
+    for (int i = 2; i <= n; i++) {
+        if (is_prime[i] && (ll)i * i <= n) {
+            for (int j = i * i; j <= n; j += i)
                 is_prime[j] = false;
         }
     }
 
-    for(ll i = 2; i <= N; i++) {
+    for(int i = 2; i <= n; i++) {
         if(is_prime[i]) {
             primes.push_back(i);
         }
