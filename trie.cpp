24af3ceb3b392ed143b51ec011147fd59e785d1e
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2023-11-06 23:10:14 +0530

    Update trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -21,14 +21,11 @@ public:
     void insert(vector<int> vect) {
         Node *temp = root;
         for(int i = 0; i < vect.size(); i++) {
-            if(temp->children.find(vect[i]) != temp->children.end()) {
-                temp = temp->children[vect[i]];
-            }
-            else {
+            if(temp->children.find(vect[i]) == temp->children.end()) {
                 Node *newNode = new Node(vect[i]);
                 temp->children[vect[i]] = newNode;
-                temp = newNode;
             }
+            temp = temp->children[vect[i]];
         }
         temp->isTerminal = true;
     }
