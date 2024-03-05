22fc198a34e87812e269d86818de27ec34957747
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2024-03-05 23:34:28 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define int long long
+#define double long double
+
+void run_case(int test_case) {
+    
+}
+
+signed main() {    
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int num_test_cases = 1;
+    cin >> num_test_cases;
+    for(int test_case = 1; test_case <= num_test_cases; test_case++) {
+        run_case(test_case);
+    }
+
+    return 0;
+}
