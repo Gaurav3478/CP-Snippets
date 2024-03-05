2e8dab4f406dfdfbcfc810b78ab15809b29ae515
Gaurav Thakur <76216479+Gaurav3478@users.noreply.github.com>
2024-03-05 23:39:37 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
+#include "ext/pb_ds/assoc_container.hpp"
+#include "ext/pb_ds/tree_policy.hpp"
 using namespace std;
+using namespace __gnu_pbds;
 using ll = long long;
 #define int long long
 #define double long double
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class key, class value, class cmp = std::less<key>>
+using ordered_map = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
 
 void run_case(int test_case) {
     
